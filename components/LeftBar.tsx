6679c843936e2684a4a1793a2ceef21c26eca612
Nhatduy209 <nhatduy20000@gmail.com>
2022-09-06 17:25:35 +0700

    update image

--- components/LeftBar.tsx
+++ components/LeftBar.tsx
@@ -9,78 +9,88 @@ import { SoundHead } from "./SoundHead";
 import { StoreHead } from "./StoreHead";
 
 export const LeftBar = () => (
-  <Container>
-    <MarginBottom20>
+  <>
+    <Container>
+      <MarginBottom20>
+        <Row>
+          <Col xs={2} sm={2}>
+            <Image
+              src="/Logo.png"
+              alt="Picture of the author"
+              width={180}
+              height={69}
+            />
+          </Col>
+          <Col xs={2} sm={2}>
+            <StoreHead />
+          </Col>
+          <Col xs={2} sm={2}>
+            <Floor />
+          </Col>
+          <Col xs={2} sm={2}>
+            <SoundHead />
+          </Col>
+          <Col xs={2} sm={2}>
+            <Open />
+          </Col>
+          <Col xs={2} sm={2}>
+            <Access />
+          </Col>
+        </Row>
+      </MarginBottom20>
+    </Container>
+
+    <MarginHorizontal>
+      <Image
+        src="/dashboard.png"
+        alt="Picture of the author"
+        width={1100}
+        height={660}
+      />
+    </MarginHorizontal>
+    <Container>
       <Row>
-        <Col xs={2} sm={2}>
-          <Image
-            src="/Logo.png"
-            alt="Picture of the author"
-            width={180}
-            height={69}
+        <Col xs={4} sm={4}>
+          <SalePannel
+            imgUrl={"/Fruit1.png"}
+            title={"レストラン・カフェ"}
+            content={
+              "2F「#ごほうびパーラー」にて7/19～の夏季限定で“#すいかパフェ”が新登場‼️"
+            }
+            dateTime={"2022.07.20"}
           />
         </Col>
-        <Col xs={2} sm={2}>
-          <StoreHead />
+        <Col xs={4} sm={4}>
+          <SalePannel
+            imgUrl={"/fruit2.png"}
+            title={"レストラン・カフェ"}
+            content={
+              "LINKS UMEDA B1F　「クリスピー・クリーム・ドーナツ」より、7/20（水）～サマーカーニバルのドーナツが発売❗️"
+            }
+            dateTime={"2022.07.20"}
+          />
         </Col>
-        <Col xs={2} sm={2}>
-          <Floor />
-        </Col>
-        <Col xs={2} sm={2}>
-          <SoundHead />
-        </Col>
-        <Col xs={2} sm={2}>
-          <Open />
-        </Col>
-        <Col xs={2} sm={2}>
-          <Access />
+        <Col xs={4} sm={4}>
+          <SalePannel
+            imgUrl={"/beauty.png"}
+            title={"ショップ"}
+            content={
+              "LINKSUMEDA3F「#VisionStyle」にて、#ゆうこす プロデュース『Chu's me』から「リリーベージュ」と「ムードグレージュ」の2色が登場💕"
+            }
+            dateTime={"2022.07.20"}
+          />
         </Col>
       </Row>
-    </MarginBottom20>
-    <Image
-      src="/dashboard.png"
-      alt="Picture of the author"
-      width={1100}
-      height={660}
-    />
-
-    <Row>
-      <Col xs={4} sm={4}>
-        <SalePannel
-          imgUrl={"/Fruit1.png"}
-          title={"レストラン・カフェ"}
-          content={
-            "2F「#ごほうびパーラー」にて7/19～の夏季限定で“#すいかパフェ”が新登場‼️"
-          }
-          dateTime={"2022.07.20"}
-        />
-      </Col>
-      <Col xs={4} sm={4}>
-        <SalePannel
-          imgUrl={"/fruit2.png"}
-          title={"レストラン・カフェ"}
-          content={
-            "LINKS UMEDA B1F　「クリスピー・クリーム・ドーナツ」より、7/20（水）～サマーカーニバルのドーナツが発売❗️"
-          }
-          dateTime={"2022.07.20"}
-        />
-      </Col>
-      <Col xs={4} sm={4}>
-        <SalePannel
-          imgUrl={"/beauty.png"}
-          title={"ショップ"}
-          content={
-            "LINKSUMEDA3F「#VisionStyle」にて、#ゆうこす プロデュース『Chu's me』から「リリーベージュ」と「ムードグレージュ」の2色が登場💕"
-          }
-          dateTime={"2022.07.20"}
-        />
-      </Col>
-    </Row>
-  </Container>
+    </Container>
+  </>
 );
 
 const Container = styled.div`
   margin: 47px 44px 39px 47px;
+`;
+
+const MarginHorizontal = styled.div`
+  margin: 0px 44px 0px 0px;
 `;
 
 const TitleText = styled.p`
